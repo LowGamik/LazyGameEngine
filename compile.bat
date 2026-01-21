2ffa441e6bd5ea59c4793ee60cf25495e505a0ba
LowGamik <lowgamik@gmail.com>
2026-01-21 20:21:19 +0100

    uhhh, I tested if SDL workds ig

--- compile.bat
+++ compile.bat
@@ -5,7 +5,7 @@ mkdir build
 
 g++ ./src/*.cpp -o build/game -Ithird_party/SDL2/include -Lthird_party/SDL2/lib -lSDL2 -Wall -Wextra
 
-xcopy .\third_party\SDL2\bin\SDL2.dll .\build\
+xcopy .\third_party\SDL2\bin\SDL2.dll .\build\ /E /I /Y
 
 ECHO tvuj program se zkompiloval
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,21 @@
 #include<SDL2/SDL.h>
 
 int main(){
-    std::cout << "ahoj svete" << std::endl;
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
+        return 1;
+    }
+
+    SDL_Window *win = SDL_CreateWindow("fak ju",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_SHOWN);
+
+    if(win == nullptr){
+        return 1;
+    }
+
+    SDL_Delay(5000);
+
+    SDL_DestroyWindow(win);
+    win = nullptr;
+
+
     return 0;
 }
